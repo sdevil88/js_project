a1ce3fc49ed0b30df5976c84d6d2cbc0a1eb676e
sagar <wwwghimiresagar88@gmail.com>
2023-08-02 08:46:28 +0545

    4th commit

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 
-let x=1;
-let y=2;
+// x= prompt("enter first number");
+// y= prompt("enter second number")
+const x=6,y=2;
 let z=x+y;
-console.log(z);
-console.log("hii");
-console.log("hello world ");
+ console.log(z);1
+ 
+if(z<=5)
+{
+    console.log("less then 5");
+    
+}
+else {
+    console.log("grater then 5");
+}
 
 
